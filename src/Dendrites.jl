0dde079ddfbe6aa2b7d0e2cdf27129dd6f2c27a3
Damien Drix <damien.drix@gmail.com>
2015-12-07 18:39:26 +0100

    fixed th adapt

--- src/Dendrites.jl
+++ src/Dendrites.jl
@@ -48,9 +48,7 @@ end
 
 learn_post(::Type{AdaptiveDendrites}) = quote
     qθ = (qplusθ * F(((g - ghyst) >= θg) & ((z_post >= θz) $ lazy_θ))
-         - qplusθ * F(((g - ghyst) < θg) & ((z_post > θz) $ lazy_θ))
          - qminθ * F(g < θg))
-#    qθ = qplusθ * F((g > (θg + θhyst)) & ((z_post > θz) $ lazy_θ)) - qminθ * F(g < θg)
     θg = clamp(θg + qθ * η, θmin, θmax)
 
     qη = I * (z_post >= θz ? -qη_dec : qη_inc) + qη_forget
